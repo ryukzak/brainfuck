0ab2285a9ab86ac420ab64ec6a32e14298ba1a5b
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2024-02-18 14:39:59 +0100

    Simplify golden test for reuse in student' works.

--- python/integration_test.py
+++ python/integration_test.py
@@ -25,7 +25,7 @@ import translator
 import translator_asm
 
 
-@pytest.mark.golden_test("golden/*.yml")
+@pytest.mark.golden_test("golden/*_bf.yml")
 def test_translator_and_machine(golden, caplog):
     """Используется подход golden tests. У него не самая удачная реализация для
     python: https://pypi.org/project/pytest-golden/ , но знать об этом подходе
@@ -62,12 +62,8 @@ def test_translator_and_machine(golden, caplog):
 
     # Создаём временную папку для тестирования приложения.
     with tempfile.TemporaryDirectory() as tmpdirname:
-        # Определяем язык golden теста. По умолчанию предполагается язык Brainfuck
-        lang = golden.get("lang", "bf")
-        assert lang == "bf" or lang == "asm"
-
         # Готовим имена файлов для входных и выходных данных.
-        source = os.path.join(tmpdirname, "source." + lang)
+        source = os.path.join(tmpdirname, "source.bf")
         input_stream = os.path.join(tmpdirname, "input.txt")
         target = os.path.join(tmpdirname, "target.o")
 
@@ -80,10 +76,7 @@ def test_translator_and_machine(golden, caplog):
         # Запускаем транслятор и собираем весь стандартный вывод в переменную
         # stdout
         with contextlib.redirect_stdout(io.StringIO()) as stdout:
-            if lang == "bf":
-                translator.main(source, target)
-            elif lang == "asm":
-                translator_asm.main(source, target)
+            translator.main(source, target)
             print("============================================================")
             machine.main(target, input_stream)
 
@@ -92,6 +85,34 @@ def test_translator_and_machine(golden, caplog):
             code = file.read()
 
         # Проверяем, что ожидания соответствуют реальности.
+        assert code == golden.out["out_code"]
+        assert stdout.getvalue() == golden.out["out_stdout"]
+        assert caplog.text == golden.out["out_log"]
+
+
+@pytest.mark.golden_test("golden/*_asm.yml")
+def test_translator_asm_and_machine(golden, caplog):
+    """Почти полная копия test_translator_and_machine."""
+    caplog.set_level(logging.DEBUG)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        source = os.path.join(tmpdirname, "source.asm")
+        input_stream = os.path.join(tmpdirname, "input.txt")
+        target = os.path.join(tmpdirname, "target.o")
+
+        with open(source, "w", encoding="utf-8") as file:
+            file.write(golden["in_source"])
+        with open(input_stream, "w", encoding="utf-8") as file:
+            file.write(golden["in_stdin"])
+
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            translator_asm.main(source, target)
+            print("============================================================")
+            machine.main(target, input_stream)
+
+        with open(target, encoding="utf-8") as file:
+            code = file.read()
+
         assert code == golden.out["out_code"]
         assert stdout.getvalue() == golden.out["out_stdout"]
         assert caplog.text == golden.out["out_log"]
